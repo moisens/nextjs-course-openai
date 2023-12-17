da551ef18189799bc65a1db7c9d51350464323d9
Moise <moise_ns@outlook.com>
2023-12-17 22:16:27 +0100

    Navbar component bug

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+const links = [
+  { id: "5ffceb2e-9a63", href: "/about", label: "about" },
+  { id: "5ffced54-9a63", href: "/client", label: "client" },
+  { id: "5ffcee80-9a63", href: "/drinks", label: "drinks" },
+  { id: "5ffcefa2-9a63", href: "/tasks", label: "tasks" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="bg-base-300 py-4">
+      <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
+        <Link href="/" className="btn btn-primary">
+          Next.js
+        </Link>
+        <ul className="menu menu-horizontal gap-2 md:ml-8">
+          {links.map((link) => {
+            const { id, href, label } = link;
+            return (
+              <li key={id}>
+                <Link href={href} className="capitalize">
+                  {label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.js
+++ app/page.js
@@ -5,9 +5,6 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about" className="text-2xl">
-        About Page
-      </Link>
     </div>
   );
 };
