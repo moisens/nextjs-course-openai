4c89910b63fb7524ad5609f49963a19ba854bf38
Moise <moise_ns@outlook.com>
2023-12-21 12:42:17 +0100

    prisma setting + more on routing

---
+++ app/(dashboard)/auth/[[...sign-in]]/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SingInPage = ({ params }) => {
+  console.log(params);
+  return <h1 className="text-7xl">Welcome on sign in page 📃</h1>;
+};
+
+export default SingInPage;
